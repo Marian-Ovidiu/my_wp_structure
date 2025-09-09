984065e54caaa5b91dcef5f9848b6f7e0b6c9d13
Marian-Ovidiu Hutanu <h.marian914@gmail.com>
2025-09-09 12:02:15 +0200

    init theme v2

---
+++ wp-content/themes/my_structure_v2/template-aziende.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ Template Name: Aziende
+ Template Post Type: page
+ */
+
+Controllers\PageController::call('aziende');
---
+++ wp-content/themes/my_structure_v2/template-progetti.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ Template Name: Progetti
+ Template Post Type: page
+ */
+
+Controllers\ProgettoController::call('archive');
