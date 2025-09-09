5fb98bc12d0f64c0efe52cc8711058b67d6e19c0
Marian-Ovidiu Hutanu <h.marian914@gmail.com>
2025-09-09 11:48:22 +0200

    init theme v2

---
+++ wp-content/themes/my_structure_v2/app/Helpers/acf_helpers.php
@@ -0,0 +1,76 @@
+<?php
+
+use Core\App;
+
+if (!function_exists('my_custom_options_page')) {
+    function my_custom_options_page()
+    {
+        add_menu_page(
+            'Impostazioni Generali',
+            'Opzioni Generali',
+            'manage_options',
+            'opzioni-generali',
+            function (){
+                my_custom_options_page_html('generali');
+            }
+        );
+        add_menu_page(
+            'Opzioni Archvio Progetto',
+            'Opzioni Archvio Progetto',
+            'manage_options',
+            'opzioni-archivio-progetto',
+            function (){
+                my_custom_options_page_html('archive-progetto');
+            }
+        );
+    }
+}
+
+if (!function_exists('my_custom_options_page_html')) {
+    function my_custom_options_page_html($page)
+    {
+        if (!current_user_can('manage_options')) {
+            return;
+        }
+
+        switch ($page) {
+            case 'generali':
+                echo App::blade()->make('optionPages.generals', [])->render();
+                break;
+            case 'archive-progetto':
+                echo App::blade()->make('optionPages.archivioOpzioniProgetto', [])->render();
+                break;
+        }
+    }
+}
+
+if (!function_exists('acf_location_rules_types')) {
+    function acf_location_rules_types( $choices ) {
+        $choices['Basic']['page'] = 'Pagina Opzioni';
+        return $choices;
+    }
+}
+
+if (!function_exists('acf_location_rule_values_page')) {
+    function acf_location_rule_values_page( $choices ) {
+        $choices['opzioni-generali'] = 'Opzioni Generali';
+        $choices['opzioni-archivio-progetto'] = 'Opzioni Archivio Progetto';
+        return $choices;
+    }
+}
+
+if (!function_exists('my_acf_location_options_page')) {
+    function my_acf_location_options_page($match, $rule, $options) {
+        if (isset($_GET['page'])) {
+            switch ($_GET['page']) {
+                case 'opzioni-generali':
+                case 'opzioni-archivio-progetto':
+                    $match = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return $match;
+    }
+}
